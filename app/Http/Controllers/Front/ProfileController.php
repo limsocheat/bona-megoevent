2a9a46a34c0c78442f959c1205651a19a7deb2e6
Lim <limsocheat111@gmail.com>
2020-06-01 15:20:30 +0700

    updated profile

--- app/Http/Controllers/Front/ProfileController.php
+++ app/Http/Controllers/Front/ProfileController.php
@@ -8,6 +8,17 @@ use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $user = Auth::user();
