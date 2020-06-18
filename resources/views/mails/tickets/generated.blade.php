99c60865f978b8ea437e4ffa3bda607eb2be6beb
laysoeun <soeunlay0715@gmail.com>
2020-06-18 12:25:26 +0700

    ss

--- resources/views/mails/tickets/generated.blade.php
+++ resources/views/mails/tickets/generated.blade.php
@@ -1,29 +1,29 @@
 @component('mail::message')
 # Ticket
 @php
-    $event  = $ticket->purchase->event;
+$event = $ticket->purchase->event;
 @endphp
 <h2 class="ml-1">{{ $event->name }}</h2>
-<p class="ml-1">{{ ($event->start_date)->format('jS F Y')}} @ {{ $event->start_time }}</p>
+<p class="ml-1">{{$event->start_date}} @ {{ $event->start_time }}</p>
 @component('mail::table')
-    <table class="table table-borderless">
-        <thead>
-            <tr>
-                <th scope="col">Ticket #</th>
-                <th scope="col">Ticket Type</th>
-                <th scope="col">Purchaser</th>
-                <th scope="col">Security Code</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>{{ $ticket->id }}</td>
-                <td>Online Purchase</td>
-                <td>{{ $ticket->purchase->user->name }}</td>
-                <td>{{ $ticket->code }}</td>
-            </tr>
-        </tbody>
-    </table>
+<table class="table table-borderless">
+    <thead>
+        <tr>
+            <th scope="col">Ticket #</th>
+            <th scope="col">Ticket Type</th>
+            <th scope="col">Purchaser</th>
+            <th scope="col">Security Code</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>{{ $ticket->id }}</td>
+            <td>Online Purchase</td>
+            <td>{{ $ticket->purchase->user->name }}</td>
+            <td>{{ $ticket->code }}</td>
+        </tr>
+    </tbody>
+</table>
 @endcomponent
 
 @component('mail::button', ['url' => route('manage.ticket.show', $ticket->id)])
