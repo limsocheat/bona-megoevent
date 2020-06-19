c1a4aabac051d565f29c6aacedb865b6a876ac4a
Lim <limsocheat111@gmail.com>
2020-06-19 15:49:45 +0700

    added ticket page

--- resources/views/front/manage/purchase/index.blade.php
+++ resources/views/front/manage/purchase/index.blade.php
@@ -3,30 +3,30 @@
 @section('title', 'Purchase')
 
 @section('content')
-    <div class="container py-4">
-        <h2>Purchase</h2>
+<div class="container py-4">
+    <h2>Purchase</h2>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Event</th>
-                    <th>User</th>
-                    <th>Quantity</th>
-                    <th>Created At</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($purchases as $purchase)
-                    <tr>
-                        <td>{{ $purchase->id }}</td>
-                        <td>{{ $purchase->event ? $purchase->event->name : null }}</td>
-                        <td>{{ $purchase->user ? $purchase->user->name : null }}</td>
-                        <td>{{ $purchase->quantity }}</td>
-                        <td>{{ $purchase->created_at }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Event</th>
+                <th>User</th>
+                <th>Quantity</th>
+                <th>Created At</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($purchases as $purchase)
+            <tr>
+                <td>{{ $purchase->id }}</td>
+                <td>{{ $purchase->event ? $purchase->event->name : null }}</td>
+                <td>{{ $purchase->user ? $purchase->user->name : null }}</td>
+                <td>{{ $purchase->quantity }}</td>
+                <td>{{ $purchase->display_created_at }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
