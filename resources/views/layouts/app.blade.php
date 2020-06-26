43aa4283e2de6ca212814842df6d233e061ec15e
Lim <limsocheat111@gmail.com>
2020-06-26 13:58:51 +0700

    added banner check and breadcrumb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -264,7 +264,7 @@
             <div class="container">
                 <ul class="breadcrumb"
                     style="margin-bottom: 0; padding-left: 0; padding-top: 3px; padding-bottom: 3px; background-color: transparent;">
-                    <li class="breadcrumb-item text-capitalize{{ $breadcrumbs->isEmpty() ? 'active' : '' }}" ><a href="/"
+                    <li class="breadcrumb-item text-capitalize{{ $breadcrumbs->isEmpty() ? 'active' : '' }}" ><a href="{{ route('index') }}"
                             style="padding: 0" >Home</a></li>
                     @foreach ($breadcrumbs as $key => $url)
                     <li class="breadcrumb-item {{ $loop->last ? 'active' : '' }}"
