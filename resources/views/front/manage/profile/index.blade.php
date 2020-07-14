81330f79c83fe8f5a3094efa90a72168db853da8
Lim Socheat <limsocheat111@gmail.com>
2020-07-14 11:36:12 +0700

    Updated

--- resources/views/front/manage/profile/index.blade.php
+++ resources/views/front/manage/profile/index.blade.php
@@ -105,7 +105,7 @@
                 </div>
                 @endif
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 mb-3">
                 <div class="card">
                     {!! Form::model($user, ['route' => ['manage.profile.update'], 'method' => 'PUT', 'files' => true])
                     !!}
@@ -161,33 +161,13 @@
                             {!! Form::text('profile[address]', null, ['placeholder' => 'Address', 'class' =>
                             'form-control']) !!}
                         </div>
+
                         <div class="form-group">
                             {!! Form::label('profile[phone]', 'Tel') !!}
-                            {!! Form::text('profile[phone]', null, ['placeholder' => 'Tel', 'class' => 'form-control'])
+                            {!! Form::text('profile[phone]', null, ['placeholder' => '(xxx)-xxx-xxxx', 'class' => 'form-control phone-format',])
                             !!}
                         </div>
 
-                        <hr>
-
-                        <div class="form-group">
-                            {!! Form::label('name', 'User Name') !!}
-                            {!! Form::text('name', null, ['placeholder' => 'user name', 'class' => 'form-control' ,'readonly' => true]) !!}
-                        </div>
-                        <div class="form-group">
-                            {!! Form::label('email', 'E-Mail Address') !!}
-                            {!! Form::email('email', null, ['placeholder' => 'enter email address', 'class' =>
-                            'form-control']) !!}
-                        </div>
-                        <div class="form-group">
-                            {!! Form::label('password', 'Password') !!}
-                            {!! Form::password('password', ['class' => 'form-control']) !!}
-                            <small id="passwordHelp" class="form-text text-muted">Leave empty to keep unchanged.</small>
-                        </div>
-                        <div class="form-group">
-                            {!! Form::label('password', 'Retype Password') !!}
-                            {!! Form::password('password-confirm', ['class' => 'form-control']) !!}
-                            <small id="passwordHelp" class="form-text text-muted">Leave empty to keep unchanged.</small>
-                        </div>
                     </div>
                     <div class="card-footer ">
                         {!! Form::submit('Save', ['class' => 'btn mego-gold-bg']); !!}
@@ -196,79 +176,108 @@
                 </div>
             </div>
 
-            <div class="col-md-6">
-
-                @if ($user->type == "company")
-                @if ($user->company)
-                <div class="card">
-                    {!! Form::model($user->company, ['route' => ['manage.company.update', $user->company->id], 'method'
-                    =>
-                    'PUT', 'files' => true ]) !!}
-                    <div class="card-body">
-                        <div class="form-group" style="height: 110px;">
-                            <div class="company-wrapper" id="profile-preview">
-                                <img id="logoPreview" class="preview-img"
-                                    src="{{ $user->company ? $user->company->logoUrl : "http://simpleicon.com/wp-content/uploads/account.png" }}"
-                                    alt="Preview Image" width="100" height="100" />
-                                <div class="logo-upload-button">
-                                    <i class="fa fa-arrow-circle-up" aria-hidden="true"></i>
-                                </div>
-                                {{ Form::file('logo', ['id' => "logoImage", "class" => "browse-input"]) }}
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            {!! Form::label('name', 'Company Name') !!}
-                            {!! Form::text('name', null, ['placeholder' => 'Company Name', 'class' => 'form-control'])
-                            !!}
-                        </div>
-                        <div class="form-group">
-                            {!! Form::label('registration_number', 'Registration Number') !!}
-                            {!! Form::text('registration_number', null, ['placeholder' => 'xxxxxxxx', 'class' =>
-                            'form-control']) !!}
-                        </div>
-                    </div>
-                    <div class="card-footer ">
-                        {!! Form::submit('Save', ['class' => 'btn mego-gold-bg']); !!}
-                    </div>
-                    {!! Form::close() !!}
-                </div>
-
-                @else
-
-                <div class="card">
-                    {!! Form::open(['route' => ['manage.company.store'], 'method' => 'POST', 'files' => true]) !!}
-                    <div class="card-body">
-                        <div class="form-group" style="height: 110px;">
-                            <div class="form-group" style="height: 110px;">
-                                <div class="company-wrapper" id="profile-preview">
-                                    <img id="logoPreview" class="preview-img"
-                                        src="http://simpleicon.com/wp-content/uploads/account.png" alt="Preview Image"
-                                        width="100" height="100" />
-                                    <div class="logo-upload-button">
-                                        <i class="fa fa-arrow-circle-up" aria-hidden="true"></i>
+            <div class="col-md-6 ">
+                <div class="col-md-12 mb-3">
+                    <div class="card">
+                        {!! Form::model($user, ['route' => ['manage.profile.update'], 'method' => 'PUT', 'files' => true])!!}
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        {!! Form::label('name', 'User Name') !!}
+                                        {!! Form::text('name', null, ['placeholder' => 'user name', 'class' => 'form-control' ,'readonly' => true]) !!}
                                     </div>
-                                    {{ Form::file('logo', ['id' => "logoImage", "class" => "browse-input"]) }}
+                                    <div class="form-group">
+                                        {!! Form::label('email', 'E-Mail Address') !!}
+                                        {!! Form::email('email', null, ['placeholder' => 'enter email address', 'class' =>'form-control' ,'readonly' => true]) !!}
+                                    </div>
+                                    <div class="form-group">
+                                        {!! Form::label('password', 'Password') !!}
+                                        {!! Form::password('password', ['class' => 'form-control']) !!}
+                                        <small id="passwordHelp" class="form-text text-muted">Leave empty to keep unchanged.</small>
+                                    </div>
+                                    <div class="form-group">
+                                        {!! Form::label('password', 'Confirm Password') !!}
+                                        {!! Form::password('password-confirm', ['class' => 'form-control']) !!}
+                                        <small id="passwordHelp" class="form-text text-muted">Leave empty to keep unchanged.</small>
+                                    </div>
+                                </div>
+                            <div class="card-footer ">
+                                {!! Form::submit('Save', ['class' => 'btn mego-gold-bg']); !!}
+                            </div>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    @if ($user->type == "company")
+                    @if ($user->company)
+                        <div class="card">
+                            {!! Form::model($user->company, ['route' => ['manage.company.update', $user->company->id], 'method'
+                            => 'PUT', 'files' => true ]) !!}
+                            <div class="card-body">
+                                <div class="form-group" style="height: 110px;">
+                                    <div class="company-wrapper" id="profile-preview">
+                                        <img id="logoPreview" class="preview-img"
+                                            src="{{ $user->company ? $user->company->logoUrl : "http://simpleicon.com/wp-content/uploads/account.png" }}"
+                                            alt="Preview Image" width="100" height="100" />
+                                        <div class="logo-upload-button">
+                                            <i class="fa fa-arrow-circle-up" aria-hidden="true"></i>
+                                        </div>
+                                        {{ Form::file('logo', ['id' => "logoImage", "class" => "browse-input"]) }}
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('name', 'Company Name') !!}
+                                    {!! Form::text('name', null, ['placeholder' => 'Company Name', 'class' => 'form-control'])
+                                    !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('registration_number', 'Registration Number') !!}
+                                    {!! Form::text('registration_number', null, ['placeholder' => 'xxxxxxxx', 'class' =>
+                                    'form-control']) !!}
                                 </div>
                             </div>
+                            <div class="card-footer ">
+                                {!! Form::submit('Save', ['class' => 'btn mego-gold-bg']); !!}
+                            </div>
+                            {!! Form::close() !!}
                         </div>
-                        <div class="form-group">
-                            {!! Form::label('name', 'Company Name') !!}
-                            {!! Form::text('name', null, ['placeholder' => 'Company Name', 'class' => 'form-control',
-                            'required' => true]) !!}
+
+                    @else
+
+                        <div class="card">
+                            {!! Form::open(['route' => ['manage.company.store'], 'method' => 'POST', 'files' => true]) !!}
+                            <div class="card-body">
+                                <div class="form-group" style="height: 110px;">
+                                    <div class="form-group" style="height: 110px;">
+                                        <div class="company-wrapper" id="profile-preview">
+                                            <img id="logoPreview" class="preview-img"
+                                                src="http://simpleicon.com/wp-content/uploads/account.png" alt="Preview Image"
+                                                width="100" height="100" />
+                                            <div class="logo-upload-button">
+                                                <i class="fa fa-arrow-circle-up" aria-hidden="true"></i>
+                                            </div>
+                                            {{ Form::file('logo', ['id' => "logoImage", "class" => "browse-input"]) }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('name', 'Company Name') !!}
+                                    {!! Form::text('name', null, ['placeholder' => 'Company Name', 'class' => 'form-control',
+                                    'required' => true]) !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('registration_number', 'Registration Number') !!}
+                                    {!! Form::text('registration_number', null, ['placeholder' => 'xxxxxxxx', 'class' =>
+                                    'form-control' , 'required' => true]) !!}
+                                </div>
+                            </div>
+                            <div class="card-footer ">
+                                {!! Form::submit('Create', ['class' => 'btn mego-gold-bg']); !!}
+                            </div>
+                            {!! Form::close() !!}
                         </div>
-                        <div class="form-group">
-                            {!! Form::label('registration_number', 'Registration Number') !!}
-                            {!! Form::text('registration_number', null, ['placeholder' => 'xxxxxxxx', 'class' =>
-                            'form-control' , 'required' => true]) !!}
-                        </div>
-                    </div>
-                    <div class="card-footer ">
-                        {!! Form::submit('Create', ['class' => 'btn mego-gold-bg']); !!}
-                    </div>
-                    {!! Form::close() !!}
+                    @endif
+                    @endif
                 </div>
-                @endif
-                @endif
             </div>
 
             <div class="col-md-12 mt-5">
@@ -319,7 +328,8 @@
         </div>
     </div>
 </div>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.1.62/jquery.inputmask.bundle.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
        var readURL = function (input) {
@@ -358,7 +368,25 @@
         $(".logo-upload-button").on('click', function () {
         $(".browse-input").click();
         });
-    })
+    });
+    /***phone number format***/
+    $(".phone-format").keypress(function (e) {
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        return false;
+        }
+        var curchr = this.value.length;
+        var curval = $(this).val();
+        if (curchr == 3 && curval.indexOf("(") <= -1) {
+        $(this).val("(" + curval + ")" + "-");
+        } else if (curchr == 4 && curval.indexOf("(") > -1) {
+        $(this).val(curval + ")-");
+        } else if (curchr == 5 && curval.indexOf(")") > -1) {
+        $(this).val(curval + "-");
+        } else if (curchr == 9) {
+        $(this).val(curval + "-");
+        $(this).attr('maxlength', '14');
+        }
+    });
 </script>
 
 @endsection
