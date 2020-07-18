2e6123e82aef887dfdefd13e1ef2a1831fc85d5c
laysoeun <soeunlay0715@gmail.com>
2020-07-18 10:05:49 +0700

    update register responsive

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,76 +2,84 @@
 
 @section('title', 'Register')
 <style>
-.form-wizard {
-  color: #888888;
-  padding: 30px;
-}
-.form-wizard .wizard-form-radio {
-  display: inline-block;
-  margin-left: 5px;
-  position: relative;
-}
-.form-wizard .wizard-form-radio input[type="radio"] {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  -ms-appearance: none;
-  -o-appearance: none;
-  appearance: none;
-  background-color: #dddddd;
-  height: 25px;
-  width: 25px;
-  display: inline-block;
-  vertical-align: middle;
-  border-radius: 50%;
-  position: relative;
-  cursor: pointer;
-}
-.form-wizard .wizard-form-radio input[type="radio"]:focus {
-  outline: 0;
-}
-.form-wizard .wizard-form-radio input[type="radio"]:checked {
-  background-color: #fb1647;
-}
-.form-wizard .wizard-form-radio input[type="radio"]:checked::before {
-  content: "";
-  position: absolute;
-  width: 10px;
-  height: 10px;
-  display: inline-block;
-  background-color: #ffffff;
-  border-radius: 50%;
-  left: 1px;
-  right: 0;
-  margin: 0 auto;
-  top: 8px;
-}
+  .form-wizard {
+    color: #888888;
+    padding: 30px;
+  }
 
-.form-wizard .wizard-form-radio input[type="radio"] ~ label {
-  padding-left: 10px;
-  cursor: pointer;
-}
-.form-wizard .form-wizard-header {
-  text-align: center;
-}
+  .form-wizard .wizard-form-radio {
+    display: inline-block;
+    margin-left: 5px;
+    position: relative;
+  }
 
-.form-wizard .wizard-fieldset {
-  display: none;
-}
-.form-wizard .wizard-fieldset.show {
-  display: block;
-}
-.form-wizard .wizard-form-error {
-  display: none;
-  background-color: #d70b0b;
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 2px;
-  width: 100%;
-}
+  .form-wizard .wizard-form-radio input[type="radio"] {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    -o-appearance: none;
+    appearance: none;
+    background-color: #dddddd;
+    height: 25px;
+    width: 25px;
+    display: inline-block;
+    vertical-align: middle;
+    border-radius: 50%;
+    position: relative;
+    cursor: pointer;
+  }
 
-/* .form-wizard .form-control {
+  .form-wizard .wizard-form-radio input[type="radio"]:focus {
+    outline: 0;
+  }
+
+  .form-wizard .wizard-form-radio input[type="radio"]:checked {
+    background-color: #fb1647;
+  }
+
+  .form-wizard .wizard-form-radio input[type="radio"]:checked::before {
+    content: "";
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    display: inline-block;
+    background-color: #ffffff;
+    border-radius: 50%;
+    left: 1px;
+    right: 0;
+    margin: 0 auto;
+    top: 8px;
+  }
+
+  .form-wizard .wizard-form-radio input[type="radio"]~label {
+    padding-left: 10px;
+    cursor: pointer;
+  }
+
+  .form-wizard .form-wizard-header {
+    text-align: center;
+  }
+
+  .form-wizard .wizard-fieldset {
+    display: none;
+  }
+
+  .form-wizard .wizard-fieldset.show {
+    display: block;
+  }
+
+  .form-wizard .wizard-form-error {
+    display: none;
+    background-color: #d70b0b;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 2px;
+    width: 100%;
+  }
+
+  /* .form-wizard .form-control {
   font-weight: 300;
   height: auto !important;
   padding: 15px;
@@ -82,11 +90,12 @@
 .form-wizard .form-control:focus {
   box-shadow: none;
 } */
-.form-wizard .form-group {
-  position: relative;
-  margin: 25px 0;
-}
-/* .form-wizard .wizard-form-text-label {
+  .form-wizard .form-group {
+    position: relative;
+    margin: 25px 0;
+  }
+
+  /* .form-wizard .wizard-form-text-label {
   position: absolute;
   left: 10px;
   top: 16px;
@@ -98,172 +107,184 @@
   transition: 0.2s linear all;
   font-size: 12px;
 } */
-.form-wizard .form-wizard-steps {
-  margin: 30px 0;
-}
-.form-wizard .form-wizard-steps li {
-  width: 25%;
-  float: left;
-  position: relative;
-}
-.form-wizard .form-wizard-steps li::after {
-  background-color: #f3f3f3;
-  content: "";
-  height: 5px;
-  left: 0;
-  position: absolute;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 100%;
-  border-bottom: 1px solid #dddddd;
-  border-top: 1px solid #dddddd;
-}
-.form-wizard .form-wizard-steps li span {
-  background-color: #dddddd;
-  border-radius: 50%;
-  display: inline-block;
-  height: 40px;
-  line-height: 40px;
-  position: relative;
-  text-align: center;
-  width: 40px;
-  z-index: 1;
-}
-/* .form-wizard .form-wizard-steps li:last-child::after {
+  .form-wizard .form-wizard-steps {
+    margin: 30px 0;
+  }
+
+  .form-wizard .form-wizard-steps li {
+    width: 25%;
+    float: left;
+    position: relative;
+  }
+
+  .form-wizard .form-wizard-steps li::after {
+    background-color: #f3f3f3;
+    content: "";
+    height: 5px;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100%;
+    border-bottom: 1px solid #dddddd;
+    border-top: 1px solid #dddddd;
+  }
+
+  .form-wizard .form-wizard-steps li span {
+    background-color: #dddddd;
+    border-radius: 50%;
+    display: inline-block;
+    height: 40px;
+    line-height: 40px;
+    position: relative;
+    text-align: center;
+    width: 40px;
+    z-index: 1;
+  }
+
+  /* .form-wizard .form-wizard-steps li:last-child::after {
   width: 50%;
 } */
-.form-wizard .form-wizard-steps li.active span, .form-wizard .form-wizard-steps li.activated span {
-  background-color: #c5b358;
-  color: #ffffff;
-}
-.form-wizard .form-wizard-steps li.active::after, .form-wizard .form-wizard-steps li.activated::after {
-  background-color: #c5b358;
-  /* left: 50%; */
-  border-color: #c5b358;
-}
-.form-wizard .form-wizard-steps li.activated::after {
-  width: 100%;
-  border-color: #c5b358;
-}
-.form-wizard .form-wizard-steps li:last-child::after {
-  left: 0;
-}
-/* .form-wizard .wizard-password-eye {
+  .form-wizard .form-wizard-steps li.active span,
+  .form-wizard .form-wizard-steps li.activated span {
+    background-color: #c5b358;
+    color: #ffffff;
+  }
+
+  .form-wizard .form-wizard-steps li.active::after,
+  .form-wizard .form-wizard-steps li.activated::after {
+    background-color: #c5b358;
+    /* left: 50%; */
+    border-color: #c5b358;
+  }
+
+  .form-wizard .form-wizard-steps li.activated::after {
+    width: 100%;
+    border-color: #c5b358;
+  }
+
+  .form-wizard .form-wizard-steps li:last-child::after {
+    left: 0;
+  }
+
+  /* .form-wizard .wizard-password-eye {
   position: absolute;
   right: 32px;
   top: 50%;
   transform: translateY(-50%);
   cursor: pointer;
 } */
-/* selecttion */
-.box {
-				color: #fff;
-				padding: 20px;
-				display: none;
-				margin-top: 20px;
-			}
-			.Individual {
-				background: #c5b358;
-			}
+  /* selecttion */
+  .box {
+    color: #fff;
+    padding: 20px;
+    display: none;
+    margin-top: 20px;
+  }
+
+  .Individual {
+    background: #c5b358;
+  }
 
 
-}
-/* email validation */
+  }
+
+  /* email validation */
 
 
 
-input {
+  input {
     width: 100%;
-}
+  }
 
-.message {
-    font: 15px ;
+  .message {
+    font: 15px;
     display: none;
     width: 100%;
     left: 0;
     color: #fff;
-}
+  }
 
-.error {
+  .error {
     margin-top: 20px;
     color: red;
     padding: 20px 0px;
-}
+  }
 
-.success {
+  .success {
     color: green;
-}
-
+  }
 </style>
 @section('content')
 
-	<div class="container py-4">
-        <div class="row justify-content-center">	
-            <div class="col-lg-8 col-md-8">
-                <div class="form-wizard">
-                    <div class="card">
-                        <div class="card-body">
-                             <div class="form-wizard-header">
-                                <h2><strong>Sign Up Your User Account</strong></h2>
-                                  <p>Fill all form field to go next step</p>
-                                  @if (count($errors) > 0)
-                                    <div class="alert alert-danger text-left">
-                                      <strong>Sorry!</strong> Please check your input again.<br><br>
-                                      <ul>
-                                        @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                        @endforeach
-                                      </ul>
-                                    </div>
-                                  @endif
-                              <ul class="list-unstyled form-wizard-steps clearfix">
-                                <li class="active"><span>1</span></li>
-                                <li><span>2</span></li>
-                                <li><span>3</span></li>
-                                <li><span>4</span></li>
-                              </ul>
-                               </div>
-                              {!! Form::open(['route' => 'register', 'method' => 'POST','role' =>'form', 'id' => 'msform', 'files' => true])!!}
-                                <fieldset class="wizard-fieldset show">
-                                    <h2>Register</h2>
-                                      @include('auth.components.singup')
-                                    <div class="form-group clearfix">
-                                        <a href="javascript:;" class=" form-wizard-next-btn btn mego-gold-bg  float-right">Next</a>
-                                    </div>
-                                </fieldset>	
-                                <fieldset class="wizard-fieldset">
-                                     <h2>Profile</h2>
-                                     @include('auth.components.profile')
-                                    <div class="form-group clearfix">
-                                        <a href="javascript:;" class="form-wizard-previous-btn btn btn btn-secondary float-left">Previous</a>
-                                        <a href="javascript:;" class="form-wizard-next-btn btn mego-gold-bg   float-right">Next</a>
-                                    </div>
-                                </fieldset>	
-                                <fieldset class="wizard-fieldset">
-                                     <h2>Company Profile</h2>
-                                    @include('auth.components.company')
-                                    <div class="form-group clearfix">
-                                        <a href="javascript:;" class="form-wizard-previous-btn btn btn btn-secondary float-left">Previous</a>
-                                         <a href="javascript:;" class="form-wizard-next-btn btn mego-gold-bg   float-right">Next</a>
-                                       
-                                </fieldset>	
-                                <fieldset class="wizard-fieldset">
-                                  <h5>Verify Information</h5>
-                                  <div class="form-group clearfix">
-                                    <a href="javascript:;" class="form-wizard-previous-btn btn btn btn-secondary float-left">Previous</a>
-                                    <button  type="submit" class="form-wizard-next-btn btn mego-gold-bg float-right">Create</button>
-                                   
-                                  </div>
-						                    </fieldset>	
-                            {!! Form::close() !!}
-                        </div>
-                    </div>
-                </div>
+<div class="container py-4">
+  <div class="row justify-content-center">
+    <div class="col-lg-8 col-md-8">
+      <div class="form-wizard mr-md-5 ml-md-5 pr-md-5 pl-md-5 p-0">
+        <div class="card">
+          <div class="card-body">
+            <div class="form-wizard-header">
+              <h2><strong>Sign Up Your User Account</strong></h2>
+              <p>Fill all form field to go next step</p>
+              @if (count($errors) > 0)
+              <div class="alert alert-danger text-left">
+                <strong>Sorry!</strong> Please check your input again.<br><br>
+                <ul>
+                  @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                  @endforeach
+                </ul>
+              </div>
+              @endif
+              <ul class="list-unstyled form-wizard-steps clearfix">
+                <li class="active"><span>1</span></li>
+                <li><span>2</span></li>
+                <li><span>3</span></li>
+                <li><span>4</span></li>
+              </ul>
             </div>
+            {!! Form::open(['route' => 'register', 'method' => 'POST','role' =>'form', 'id' => 'msform', 'files' =>
+            true])!!}
+            <fieldset class="wizard-fieldset show">
+              <h2>Register</h2>
+              @include('auth.components.singup')
+              <div class="form-group clearfix">
+                <a href="javascript:;" class=" form-wizard-next-btn btn mego-gold-bg  float-right">Next</a>
+              </div>
+            </fieldset>
+            <fieldset class="wizard-fieldset">
+              <h2>Profile</h2>
+              @include('auth.components.profile')
+              <div class="form-group clearfix">
+                <a href="javascript:;" class="form-wizard-previous-btn btn btn btn-secondary float-left">Previous</a>
+                <a href="javascript:;" class="form-wizard-next-btn btn mego-gold-bg   float-right">Next</a>
+              </div>
+            </fieldset>
+            <fieldset class="wizard-fieldset">
+              <h2>Company Profile</h2>
+              @include('auth.components.company')
+              <div class="form-group clearfix">
+                <a href="javascript:;" class="form-wizard-previous-btn btn btn btn-secondary float-left">Previous</a>
+                <a href="javascript:;" class="form-wizard-next-btn btn mego-gold-bg   float-right">Next</a>
+
+            </fieldset>
+            <fieldset class="wizard-fieldset">
+              <h5>Verify Information</h5>
+              <div class="form-group clearfix">
+                <a href="javascript:;" class="form-wizard-previous-btn btn btn btn-secondary float-left">Previous</a>
+                <button type="submit" class="form-wizard-next-btn btn mego-gold-bg float-right">Create</button>
+
+              </div>
+            </fieldset>
+            {!! Form::close() !!}
+          </div>
         </div>
-	</div>
-	
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 
